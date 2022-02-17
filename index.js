a0d1caa32e99f79007c629605bf84cbb6412b012
Armen <armen.yegoryan97@gmail.com>
2022-02-17 19:40:58 +0400

    bug fixed

--- index.js
+++ index.js
@@ -10,33 +10,36 @@ if(!fs.statSync(dirPath).isDirectory()) {
 }
 let fileArr = [];
 function filePaths(dirPath) {
-    let data = fs.readdirSync(dirPath);
-    data.forEach((val, index) => {
-        let filePath = path.join(dirPath, val);
-        if(fs.statSync(filePath).isDirectory()) {
-            filePaths(filePath);
-        } else {
-            fileArr.push(filePath);
+    fs.readdir(dirPath, (err, data) => {
+        if(err) {
+            console.log(err);
         }
-    })
+        data.forEach((val, index) => {
+            let filePath = path.join(dirPath, val);
+            if(fs.statSync(filePath).isDirectory()) {
+                filePaths(filePath);
+            } else {
+                fileArr.push(filePath);
+            }
+        });
+        for(let i = 0; i < fileArr.length; ++i) {
+            for(let j = 0; j < fileArr.length - 1; ++j) {
+                if(fs.statSync(fileArr[j]).size > fs.statSync(fileArr[j + 1]).size) {
+                    let temp = fileArr[j];
+                    fileArr[j] = fileArr[j + 1];
+                    fileArr[j + 1] = temp;
+                }
+            }
+        }
+        
+        let ws = fs.createWriteStream("sorted_file.txt", {
+        });
+        
+        for(let i = 0; i < fileArr.length; ++i) {
+            let text = fileArr[i] + " ----- " + fs.statSync(fileArr[i]).size + "b" + "\n";
+            ws.write(text);
+        }
+    });
 }
 
 filePaths(dirPath);
-
-for(let i = 0; i < fileArr.length; ++i) {
-    for(let j = 0; j < fileArr.length - 1; ++j) {
-        if(fs.statSync(fileArr[j]).size > fs.statSync(fileArr[j + 1]).size) {
-            let temp = fileArr[j];
-            fileArr[j] = fileArr[j + 1];
-            fileArr[j + 1] = temp;
-        }
-    }
-}
-
-let ws = fs.createWriteStream("sorted_file.txt", {
-});
-
-for(let i = 0; i < fileArr.length; ++i) {
-    let text = fileArr[i] + " ----- " + fs.statSync(fileArr[i]).size + "b" + "\n";
-    ws.write(text);
-}
